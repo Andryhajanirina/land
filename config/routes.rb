78d4cc28946f82362d46b998ad87bc6e2cccc808
Andryhajanirina <andryhaj@gmail.com>
2018-02-23 02:51:21 +0300

    Modification template

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :events
   #get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root to: 'home#index'
+  root to: 'home#index'
 
 end
